fc2f1756e2773da2e56f34943f4b3939a86407a9
Miten Gajjar <miten3377@gamil.com>
2021-11-08 15:20:32 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export const getStaticProps = async () => {
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+
+    const entries = await client.getEntries({
+        content_type: "recipe",
+    });
+
+    return {
+        props: {
+            recipes: entries.items,
+        },
+    };
+};
+
+export default function Recipes({ recipes }) {
+    return (
+        <div className="recipe-list">
+            {recipes.map((recipe) => {
+                return <RecipeCard recipe={recipe} key={recipe.sys.id} />;
+            })}
+
+            <style jsx>
+                {`
+                    .recipe-list {
+                        display: grid;
+                        grid-template-columns: 1fr 1fr;
+                        grid-gap: 20px;
+                    }
+                `}
+            </style>
+        </div>
+    );
 }
