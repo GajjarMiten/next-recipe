98340a5336d9e3cedfb5e8b5a331e706150d2ef9
Miten Gajjar <miten3377@gamil.com>
2021-11-15 21:41:35 +0530

    fallback component

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,8 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 
+import Skeleton from "../../components/Skeleton";
+
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
@@ -23,7 +25,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     };
 };
 
@@ -42,6 +44,10 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+    if (!recipe) {
+        return <Skeleton />;
+    }
+
     const { title, ingredients, featureImage, method, cookingTime } =
         recipe.fields;
 
